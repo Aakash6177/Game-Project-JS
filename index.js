cd1fbf44918fad1d65eb5c6468065f621e6f0feb
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-08-04 03:25:03 -0700

    updated the flickering problem

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = "./images/shadow_dog.png";
-const spwidth = 575;
-const spheight =  523;
+const spriteWidth = 575;
+const spriteHeight =  523;
 let frameX = 0;
 let frameY = 0;
-
+let gameFrame = 0;
+let staggerFrame = 5;
 function animate(){
   ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(0,50,100,100);
   // sx, sy, sw, sh, dx, dy, dw, dh --> this gives the source x-y coordinates and source width and height
   // same goes for the destination --> draw function (image object, sx,sy, sw, sh, dx, dy, dw, dh);
 
-  ctx.drawImage(playerImage, frameX * spwidth, frameY * spheight, spwidth, spheight, 0, 0, spwidth, spheight);
-  if(frameX < 6) frameX++;
-  else frameX = 0;
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame% staggerFrame == 0){
+    if(frameX < 6) frameX = frameX+ 1;
+    else frameX = 0;
+  }
+
+
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
