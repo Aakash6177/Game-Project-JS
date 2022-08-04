fa0f3a855dd4db303a914024b56bb417605b26bc
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-08-04 02:57:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = "./images/shadow_dog.png";
+const spwidth = 575;
+const spheight =  523;
+let frameX = 0;
+let frameY = 0;
+
 function animate(){
   ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(0,50,100,100);
-  ctx.drawImage(playerImage, 0, 0, 600, 600, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.fillRect(0,50,100,100);
+  // sx, sy, sw, sh, dx, dy, dw, dh --> this gives the source x-y coordinates and source width and height
+  // same goes for the destination --> draw function (image object, sx,sy, sw, sh, dx, dy, dw, dh);
 
+  ctx.drawImage(playerImage, frameX * spwidth, frameY * spheight, spwidth, spheight, 0, 0, spwidth, spheight);
+  if(frameX < 6) frameX++;
+  else frameX = 0;
   requestAnimationFrame(animate);
 }
 animate();
